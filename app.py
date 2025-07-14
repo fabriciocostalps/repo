4a08a2eee44b5d52df905fdcce7830ac569941b5
fabriciocostalps <fabriciocostalps06@gmail.com>
2025-07-14 09:19:32 -0300

    carrega imagem

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import gdown
 import tensorflow as tf
+import io
+from PIL import image
+import numpy as np
+import pandas as pd
+import plotly.express as px
 
+@st.cache_resource
 def carrega_modelo():
     #https://drive.google.com/file/d/1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp/view?usp=sharing
     url = 'https://drive.google.com/uc?id=1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp'
@@ -14,9 +20,34 @@ def carrega_modelo():
     # disponibiliza para uso
     interpreter.allocate_tenors()
 
-    return interpretor
+    return interpreter
+
+def carrega_imagem():
+    uploaded_file = st.file_uploader("Arraste e solte a uma imagem ou clique para selizionar uma , type=['png', 'jpg', 'jpeg'])
+
+    if uploaded_file is not None:
+        image_data = uploaded_file.read()
+
+        # abrir a imagem
+        image = image.open(io.Bytes10(image_data))
+
+        # exibir a imagem na página
+        st.image(image)
+        st.sucess('Imagem foi carregada com sucesso!')
+
+        # converter a imagem em ponto flutuante
+        image = np.array(image, dtype=np.float32)
+
+        # normalizar a imagem
+        image = image / 255.0
+
+        # adicionar uma dimensão extra
+        image = np.expand_dims(image,  axis=0)
+        
+        return image
 
 def main():
+
     st.set_page_config(
         page_title = "Classifica folhas de videiras!"
     )
@@ -26,9 +57,10 @@ def main():
 
 
     # Carregar o modelo
+    interpreter = carrega_modelo()
 
     # Carregar a imagem
-
+    imagem = carrega_imagem()
     # Classificar a imagem
 
 
