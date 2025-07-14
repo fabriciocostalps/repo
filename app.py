28e346e56dab77ff12e6565d1e944bb336392d0d
fabriciocostalps <fabriciocostalps06@gmail.com>
2025-07-14 09:38:12 -0300

    sad

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def carrega_modelo():
     return interpreter
 
 def carrega_imagem():
-    uploaded_file = st.file_uploader("Arraste e solte uma imagem ou clique para selecionar", type=['png', 'jpg', 'jpeg'])
+    uploaded_file = st.file_uploader("Arraste e solte uma imagem ou clique para selecionar.", type=['png', 'jpg', 'jpeg'])
 
     if uploaded_file is not None:
         image_data = uploaded_file.read()
