bb7eb088973a66013580691c84bf3ac95af60be2
fabriciocostalps <fabriciocostalps06@gmail.com>
2025-07-14 08:39:32 -0300

    app.py criado

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import streamlit as st 
+
+    def main ():
+        st.set_page_config(
+            pag_title="Classifica Folhas de Videiras!")
+
+        st.write("# Classifica Folhas de Videiras!")
+
+        # carregar o modelo
+
+        # carregar a imagem
+
+        # classificar a imagem
+
+        if __name__ == "__main__":
+            main()
