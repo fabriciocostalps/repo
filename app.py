ac29628607283eb3024982e6cea9924feb78de17
fabriciocostalps <fabriciocostalps06@gmail.com>
2025-07-14 09:26:59 -0300

    teste

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import gdown
 import tensorflow as tf
 import io
-from PIL import image
+from PIL import Image
 import numpy as np
 import pandas as pd
 import plotly.express as px
